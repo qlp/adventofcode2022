6d77aca5e469ba9b16ac262e9859f0465014eae9
Jurriaan Pruijs <jpruijs@bol.com>
2022-12-04 19:13:12 +0100

    Day 2; first assignment, single line challenge

--- nl/q8p/aoc2022/day02/Day02.java
+++ nl/q8p/aoc2022/day02/Day02.java
@@ -10,26 +10,10 @@ public class Day02 implements Day {
     @Override
     public Assignment first() {
         return input -> stream(input.split("\\n")) // plays
-                .map(play ->play.split(" ")) // two strings in an array
+                .map(play -> stream(play.split(" ")).mapToInt(s -> s.charAt(0)).toArray()) // two chars in an array
                 .mapToInt(play ->
-                        switch (play[1]) {
-                            case "X" -> 1 + switch (play[0]) {
-                                case "A" -> 3;
-                                case "B" -> 0;
-                                default -> 6;
-                            };
-                            case "Y" -> 2 + switch (play[0]) {
-                                case "A" -> 6;
-                                case "B" -> 3;
-                                default -> 0;
-                            };
-                            default -> 3 + switch (play[0]) {
-                                case "A" -> 0;
-                                case "B" -> 6;
-                                default -> 3;
-                            };
-                        }
-                )
+                        play[1] - 'X' + 1 + // points for the move
+                        (((play[1] - 'X') - (play[0] - 'A') + 3 + 1) % 3) * 3) // points for winning, loosing or draw
                 .sum();
     }
 
