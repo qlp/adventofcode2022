71ea20b5e67124510e2d49bb85cc82ed0da1284b
Jurriaan Pruijs <jpruijs@bol.com>
2022-12-06 09:02:01 +0100

    Improve Day06 - using streams

--- nl/q8p/aoc2022/day06/Day06.java
+++ nl/q8p/aoc2022/day06/Day06.java
@@ -4,28 +4,23 @@ import nl.q8p.aoc2022.Assignment;
 import nl.q8p.aoc2022.Day;
 
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day06 implements Day {
 
     @Override
     public Assignment first() {
-        return input -> {
-            var i = 4;
-
-            while (input.substring(i - 4, i).chars().boxed().collect(Collectors.toSet()).size() != 4) i++;
-
-            return i;
-        };
+        return input -> IntStream.range(4, input.length())
+            .filter(i -> input.substring(i - 4, i).chars().boxed().collect(Collectors.toSet()).size() == 4)
+            .findFirst()
+            .orElseThrow();
     }
 
     @Override
     public Assignment second() {
-        return input -> {
-            var i = 14;
-
-            while (input.substring(i - 14, i).chars().boxed().collect(Collectors.toSet()).size() != 14) i++;
-
-            return i;
-        };
+        return input -> IntStream.range(14, input.length())
+            .filter(i -> input.substring(i - 14, i).chars().boxed().collect(Collectors.toSet()).size() == 14)
+            .findFirst()
+            .orElseThrow();
     }
 }
