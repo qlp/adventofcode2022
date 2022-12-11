7bc9e917e404779d651c063051850f36492e6dc6
Jurriaan Pruijs <jpruijs@bol.com>
2022-12-11 11:51:46 +0100

    Day 11 - refactor with iterator

--- nl/q8p/aoc2022/day11/Day11.java
+++ nl/q8p/aoc2022/day11/Day11.java
@@ -3,20 +3,50 @@ package nl.q8p.aoc2022.day11;
 import nl.q8p.aoc2022.Assignment;
 import nl.q8p.aoc2022.Day;
 
-import java.math.BigInteger;
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
-import java.util.logging.Logger;
 import java.util.stream.IntStream;
 
-public class Day11 implements Day {
-    private static final Logger LOG = Logger.getLogger(Day11.class.getName());
+import static java.util.Comparator.reverseOrder;
 
-    record Monkey(LinkedList<BigInteger> items, Function<BigInteger, BigInteger> inspect, BigInteger test, int ifTrue, int ifFalse, AtomicLong inspectedCount) {
+public class Day11 implements Day {
+    record Monkey(LinkedList<Long> items, Function<Long, Long> inspect, Long test, int ifTrue, int ifFalse, AtomicLong inspectedCount)
+    implements Iterator<Long> {
+        static Monkey parse(String string) {
+            var parts = string.split("\\n");
+            return new Monkey(
+                new LinkedList<>(Arrays.stream(parts[1].split(": ")[1].split(", ")).map(Long::parseLong).toList()),
+                monkeyFunction(parts[2].split(" = old ")[1]),
+                Long.parseLong(parts[3].split(" by ")[1]),
+                Integer.parseInt(parts[4].split(" monkey ")[1]),
+                Integer.parseInt(parts[5].split(" monkey ")[1]),
+                new AtomicLong()
+            );
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !items.isEmpty();
+        }
+
+        @Override
+        public Long next() {
+            inspectedCount.incrementAndGet();
+
+            return inspect.apply(items.removeLast());
+        }
+
+        public int target(long bored) {
+            return bored % test == 0L ? ifTrue : ifFalse;
+        }
+
+        public void add(long item) {
+            items.add(item);
+        }
     }
 
     @Override
@@ -29,67 +59,38 @@ public class Day11 implements Day {
         return input -> playGame(input, 10000, 1);
     }
 
+    private static long playGame(String input, int numberOfRounds, long boredDivisor) {
+        var monkeys = Arrays.stream(input.split("\\n\\n")).map(Monkey::parse).toList();
 
-    private static long playGame(String input, int numberOfRounds, int boredDevisor) {
-        var monkeys = parse(input);
+        IntStream.range(0, numberOfRounds).forEach(i -> round(monkeys, boredDivisor));
 
-        IntStream.range(0, numberOfRounds).forEach(i -> {
-            playRound(monkeys, BigInteger.valueOf(boredDevisor));
-
-            LOG.info("round " + i);
-        });
-
-        return monkeys.stream().mapToLong(m -> m.inspectedCount.get()).boxed().sorted(Comparator.reverseOrder()).mapToLong(i -> i).limit(2).reduce(1L, (a, b) -> a * b);
+        return monkeys.stream()
+                .mapToLong(m -> m.inspectedCount.get())
+                .boxed()
+                .sorted(reverseOrder())
+                .mapToLong(i -> i)
+                .limit(2)
+                .reduce(1L, (a, b) -> a * b);
     }
 
-    private static List<Monkey> parse(String input) {
-        return Arrays.stream(input
-                        .split("\\n\\n"))
-                .map(monkey -> new Monkey(
-                                new LinkedList<>(Arrays.stream(monkey.split("\\n")[1].split(": ")[1].split(", ")).map(BigInteger::new).toList()),
-                                monkeyFunction(monkey.split("\\n")[2].split(" = old ")[1]),
-                                new BigInteger(monkey.split("\\n")[3].split(" by ")[1]),
-                                Integer.parseInt(monkey.split("\\n")[4].split(" monkey ")[1]),
-                                Integer.parseInt(monkey.split("\\n")[5].split(" monkey ")[1]),
-                                new AtomicLong()
-                        )
-                ).toList();
+    private static void round(List<Monkey> monkeys, Long boredDivisor) {
+        var max = monkeys.stream().map(m -> m.test).reduce(1L, (a, b) -> a * b);
+
+        monkeys.forEach(monkey -> monkey.forEachRemaining(inspected -> {
+            var stress = (inspected / boredDivisor) % max;
+
+            monkeys.get(monkey.target(stress)).add(stress);
+        }));
     }
 
-    private static void playRound(List<Monkey> monkeys, BigInteger boredDevisor) {
-        monkeys.forEach(monkey -> {
-            while(!monkey.items.isEmpty()) {
-                var next = monkey.items.removeFirst();
+    public static Function<Long, Long> monkeyFunction(String function) {
+        var parts = function.split(" ");
+        var value = parts[1];
 
-                monkey.inspectedCount.incrementAndGet();
-
-                var inspected = monkey.inspect().apply(next);
-                var bored = inspected;
-
-                if (!boredDevisor.equals(BigInteger.ONE)) {
-                    bored = inspected.divide(boredDevisor);
-                }
-
-                var target = bored.mod(monkey.test).equals(BigInteger.ZERO) ? monkey.ifTrue : monkey.ifFalse;
-
-                monkeys.get(target).items.add(bored);
-            }
-        });
-    }
-
-    public static Function<BigInteger, BigInteger> monkeyFunction(String function) {
-        if (function.equals("* old")) {
-            return old -> old.pow(2);
-        } else {
-            var parts = function.split(" ");
-            var opration = parts[0];
-            var value = new BigInteger(parts[1]);
-
-            return switch(opration) {
-                case "+" -> old -> old.add(value);
-                case "*" -> old -> old.multiply(value);
-                default -> throw new UnsupportedOperationException(function);
-            };
-        }
+        return switch(parts[0]) {
+            case "+" -> old -> old + Integer.parseInt(value);
+            case "*" -> old -> old * (value.equals("old") ? old : Integer.parseInt(value));
+            default -> throw new UnsupportedOperationException(parts[0]);
+        };
     }
 }
