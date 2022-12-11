7e8c3e774b94d25f3743d595262b5e49d7ff728b
Jurriaan Pruijs <jpruijs@bol.com>
2022-12-11 09:19:24 +0100

    Day 11 - part 2 (some optimizations)

--- nl/q8p/aoc2022/day11/Day11.java
+++ nl/q8p/aoc2022/day11/Day11.java
@@ -10,9 +10,11 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
+import java.util.logging.Logger;
 import java.util.stream.IntStream;
 
 public class Day11 implements Day {
+    private static final Logger LOG = Logger.getLogger(Day11.class.getName());
 
     record Monkey(LinkedList<BigInteger> items, Function<BigInteger, BigInteger> inspect, BigInteger test, int ifTrue, int ifFalse, AtomicLong inspectedCount) {
     }
@@ -24,14 +26,18 @@ public class Day11 implements Day {
 
     @Override
     public Assignment second() {
-        return input -> playGame(input, 700, 1);
+        return input -> playGame(input, 10000, 1);
     }
 
 
     private static long playGame(String input, int numberOfRounds, int boredDevisor) {
         var monkeys = parse(input);
 
-        IntStream.range(0, numberOfRounds).forEach(i -> playRound(monkeys, BigInteger.valueOf(boredDevisor)));
+        IntStream.range(0, numberOfRounds).forEach(i -> {
+            playRound(monkeys, BigInteger.valueOf(boredDevisor));
+
+            LOG.info("round " + i);
+        });
 
         return monkeys.stream().mapToLong(m -> m.inspectedCount.get()).boxed().sorted(Comparator.reverseOrder()).mapToLong(i -> i).limit(2).reduce(1L, (a, b) -> a * b);
     }
@@ -41,7 +47,7 @@ public class Day11 implements Day {
                         .split("\\n\\n"))
                 .map(monkey -> new Monkey(
                                 new LinkedList<>(Arrays.stream(monkey.split("\\n")[1].split(": ")[1].split(", ")).map(BigInteger::new).toList()),
-                                new MonkeyFunction(monkey.split("\\n")[2].split(" = ")[1]),
+                                monkeyFunction(monkey.split("\\n")[2].split(" = old ")[1]),
                                 new BigInteger(monkey.split("\\n")[3].split(" by ")[1]),
                                 Integer.parseInt(monkey.split("\\n")[4].split(" monkey ")[1]),
                                 Integer.parseInt(monkey.split("\\n")[5].split(" monkey ")[1]),
@@ -58,7 +64,11 @@ public class Day11 implements Day {
                 monkey.inspectedCount.incrementAndGet();
 
                 var inspected = monkey.inspect().apply(next);
-                var bored = inspected.divide(boredDevisor);
+                var bored = inspected;
+
+                if (!boredDevisor.equals(BigInteger.ONE)) {
+                    bored = inspected.divide(boredDevisor);
+                }
 
                 var target = bored.mod(monkey.test).equals(BigInteger.ZERO) ? monkey.ifTrue : monkey.ifFalse;
 
@@ -67,23 +77,19 @@ public class Day11 implements Day {
         });
     }
 
-    record MonkeyFunction(String function) implements Function<BigInteger, BigInteger> {
-        @Override
-        public BigInteger apply(BigInteger old) {
-            var operation = function.split(" ");
-            var left = valueOf(operation[0], old);
-            var function = operation[1];
-            var right = valueOf(operation[2], old);
+    public static Function<BigInteger, BigInteger> monkeyFunction(String function) {
+        if (function.equals("* old")) {
+            return old -> old.pow(2);
+        } else {
+            var parts = function.split(" ");
+            var opration = parts[0];
+            var value = new BigInteger(parts[1]);
 
-            return switch(function) {
-                case "+" -> left.add(right);
-                case "*" -> left.multiply(right);
+            return switch(opration) {
+                case "+" -> old -> old.add(value);
+                case "*" -> old -> old.multiply(value);
                 default -> throw new UnsupportedOperationException(function);
             };
-        }
-
-        public BigInteger valueOf(String string, BigInteger old) {
-            return string.equals("old") ? old : new BigInteger(string);
         }
     }
 }
