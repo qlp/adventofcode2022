25c00de502fd1d05ff12a7879b3e918065f09baa
Jurriaan Pruijs <jurriaan@pruijs.nl>
2022-12-15 23:18:33 +0100

    Verified?

--- nl/q8p/aoc2022/Main.java
+++ nl/q8p/aoc2022/Main.java
@@ -16,6 +16,7 @@ public class Main {
     private static final Logger LOG = Logger.getLogger(Main.class.getName());
 
     private final String assignmentFilter;
+
     private final String runFilter;
 
     public Main(String assignmentFilter, String runFilter) {
