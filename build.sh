bee92c25c1dc3b72d24d4fe22bafe6609d781cd2
Jurriaan Pruijs <jurriaan@pruijs.nl>
2022-12-15 23:58:56 +0100

    Verified? (12)

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@ mkdir -p out/target 2> /dev/null
 find nl -type f -name "*.java" > out/sources.txt
 find nl -type f ! -name "*.java" > out/resources.txt
 
-
 javac -d ./out/target @out/sources.txt
 rsync -a . --files-from=out/resources.txt out/target
 cp logging.properties out/target
