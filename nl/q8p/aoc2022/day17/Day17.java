5f9e4cc4c96180ff4a1d6b0adcbd9f62b201a34c
Jurriaan Pruijs <jurriaan@pruijs.nl>
2022-12-17 18:01:10 +0100

    Day 17 - second - save work of refactor to int model

--- nl/q8p/aoc2022/day17/Day17.java
+++ nl/q8p/aoc2022/day17/Day17.java
@@ -5,15 +5,43 @@ import nl.q8p.aoc2022.Day;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
-import java.util.stream.Collector;
 import java.util.stream.IntStream;
 
 public class Day17 implements Day {
 
     private static final Logger LOG = Logger.getLogger(Day17.class.getName());
+
+
+    static final String AIR = ".";
+    static final String VOID = ".";
+    static final String WALL = "|";
+    static final String BOTTOM = "-";
+    static final String BLOCK = "#";
+    static final String CORNER = "+";
+
+    static final char CURRENT_CHAR = '@';
+
+    static final String CURRENT = "" + CURRENT_CHAR;
+
+    static String UNMOVABLE = WALL + BOTTOM + BLOCK + CORNER;
+
+    private static int valueForString(String update) {
+        int result = 0;
+
+        String value = update.substring(1, update.length() - 1);
+
+        for (int i = 0; i < value.length(); i++) {
+            result |= UNMOVABLE.indexOf(value.charAt(i)) != -1 ? 1 << i : 0;
+        }
+
+        return result;
+    }
+
 
     enum BlockType {
         DASH(new String[] { "@@@@" }),
@@ -22,14 +50,17 @@ public class Day17 implements Day {
         PIPE(new String[] { "@", "@", "@", "@" }),
         BLOCK(new String[] { "@@", "@@" });
 
-        private final String[] shape;
+        private final int[] shape;
+
+        private final int width;
 
         int height() {
             return shape.length;
         }
 
         BlockType(String[] shape) {
-            this.shape = shape;
+            this.shape = Arrays.stream(shape).mapToInt(string -> valueForString(" " + string.replace('@', '#') + " ")).toArray();
+            this.width = shape[0].length();
         }
 
         static BlockType first() {
@@ -91,23 +122,16 @@ public class Day17 implements Day {
     }
 
     static class Cave {
-        LinkedList<String> rows = new LinkedList<>();
+        LinkedList<Integer> rows = new LinkedList<>();
+
+        long removedToOptimize = 0L;
 
         int width = 7;
         int emptyRowsAboveStack = 3;
 
-        static final String AIR = ".";
-        static final String VOID = ".";
-        static final String WALL = "|";
-        static final String BOTTOM = "-";
-        static final String BLOCK = "#";
-        static final String CORNER = "+";
+        private final int bottom = 127;
 
-        static final char CURRENT_CHAR = '@';
-
-        static final String CURRENT = "" + CURRENT_CHAR;
-
-        static String UNMOVABLE = WALL + BOTTOM + BLOCK + CORNER;
+        private final int empty = 0;
 
         String emptyRow = AIR.repeat(width);
         String voidRow = VOID.repeat(width);
@@ -119,6 +143,8 @@ public class Day17 implements Day {
 
         private Block current;
 
+        private Map<String, Integer> unmovableCalculations = new HashMap<>();
+
         public Cave(Wind wind) {
             this.wind = wind;
 
@@ -127,15 +153,23 @@ public class Day17 implements Day {
 
         void addBlock(BlockType blockType) {
             removeEmptyRowsAtTheTop();
+            removeRedundantRowsAtTheBottom();
 
             this.current = new Block(blockType, new Position(rows.size() + blockType.height() + emptyRowsAboveStack - 1));
         }
 
-        void removeEmptyRowsAtTheTop() {
+        private void removeRedundantRowsAtTheBottom() {
+            if (rows.size() > 500) {
+                rows = new LinkedList<>(rows.subList(100, rows.size()));
+                removedToOptimize += 100;
+            }
+        }
+
+        private void removeEmptyRowsAtTheTop() {
             for (int rowNumber = rows.size(); rowNumber >= 0; rowNumber--) {
-                if(rowNumber == 0 || !rows.get(rowNumber - 1).equals(emptyRow)) {
+                if(rowNumber == 0 || rows.get(rowNumber - 1) != 0) {
                     if (rowNumber > rows.size()) {
-                        IntStream.range(0, rowNumber - rows.size()).forEach(i -> rows.add(emptyRow));
+                        IntStream.range(0, rowNumber - rows.size()).forEach(i -> rows.add(empty));
                     } else {
                         IntStream.range(0, rows.size() - rowNumber).forEach(i -> rows.removeLast());
                     }
@@ -176,7 +210,13 @@ public class Day17 implements Day {
         }
 
         private int countImmutables(String[] stringsAroundBlock) {
-            return Arrays.stream(stringsAroundBlock).mapToInt(line -> (int)line.chars().filter(c -> UNMOVABLE.indexOf(c) != -1).count()).sum();
+            var sum = 0;
+
+            for (int i = 0; i < stringsAroundBlock.length; i++) {
+                sum += unmovableCalculations.computeIfAbsent(stringsAroundBlock[i], s -> (int)s.chars().filter(c -> UNMOVABLE.indexOf(c) != -1).count());
+            }
+
+            return sum;
         }
 
         @Override
@@ -200,27 +240,49 @@ public class Day17 implements Day {
             return result;
         }
 
+        private int[] valuesAroundBlock(Block position, Block representation) {
+            var result = new int[position.type.height() + 1];
+
+            for (int y = 0; y < position.type.height() + 1; y++) {
+                result[y] = valueForLine(position.position.y - y, representation);
+            }
+
+            return result;
+        }
+
         private void save(Block block) {
             for (int y = block.type.height() - 1; y >= 0; y--) {
                 var rowIndex = block.position.y - y;
 
                 if (rows.size() < rowIndex + 1) {
-                    rows.add(emptyRow);
+                    rows.add(empty);
                 }
 
                 var update = stringForLine(rowIndex, block).replace(CURRENT, BLOCK).replace(VOID, AIR);
-                rows.set(rowIndex, update.substring(1, update.length() - 1));
+                rows.set(rowIndex, valueForString(update));
             }
+        }
+
+        private String stringForValue(int value) {
+            return String.valueOf(new char[] {
+                    (value & 1) == 1 ? '#' : ' ',
+                    (value & 2) == 2 ? '#' : ' ',
+                    (value & 4) == 4 ? '#' : ' ',
+                    (value & 8) == 8 ? '#' : ' ',
+                    (value & 16) == 16 ? '#' : ' ',
+                    (value & 32) == 32 ? '#' : ' ',
+                    (value & 64) == 64 ? '#' : ' '
+            });
         }
 
         private String stringForLine(int y, Block block) {
             String result;
             if (y < 0) {
-                result = CORNER + bottomRow + CORNER;
+                result = CORNER + stringForValue(bottom) + CORNER;
             } else if (y < rows.size()) {
-                result = WALL + rows.get(y) + WALL;
+                result = WALL + stringForValue(rows.get(y)) + WALL;
             } else {
-                result = WALL + voidRow + WALL;
+                result = WALL + stringForValue(empty) + WALL;
             }
 
             int blockTop = block.position.y;
@@ -229,14 +291,14 @@ public class Day17 implements Day {
             if (y <= blockTop && y >= blockBottom) {
                 int blockLineIndex = blockTop - y;
 
-                String blockString = block.type.shape[blockLineIndex];
+                String blockString = stringForValue(block.type.shape[blockLineIndex]);
 
                 StringBuilder withBlock = new StringBuilder(result);
 
-                for (int x = 0; x < blockString.length(); x++) {
+                for (int x = 0; x < block.type.width; x++) {
                     var c = blockString.charAt(x);
 
-                    if (c == CURRENT_CHAR) {
+                    if (c == '#') {
                         withBlock.setCharAt(block.position.x + x, CURRENT_CHAR);
                     }
                 }
@@ -247,15 +309,28 @@ public class Day17 implements Day {
             return result;
         }
 
+        private int valueForLine(int y, Block block) {
+            return valueForString(stringForLine(y, block)); // <<< TODO: optimize
+        }
+
         public long height() {
-            return rows.size();
+            return removedToOptimize + rows.size();
         }
 
         public long heightAfter(long blockCount) {
             var blockCounter = 0L;
 
+            var tickCounter = 0L;
+
             while(blockCounter != blockCount) {
+//                LOG.info(this::toString);
+                tickCounter++;
                 blockCounter += tick() ? 1 : 0;
+
+
+                if (tickCounter % 1_000_000 == 0) {
+                    LOG.info("" + tickCounter + ": " + blockCounter + " / " + blockCount + " (" + (blockCounter / blockCount * 100L) + "%)");
+                }
             }
 
             return height();
