5f602a016bd75315b9d03139c8d08b86e6745a94
Jurriaan Pruijs <jurriaan@pruijs.nl>
2022-12-23 14:01:29 +0100

    Day 24 - prepare

---
+++ nl/q8p/aoc2022/day24/Day24.java
@@ -0,0 +1,17 @@
+package nl.q8p.aoc2022.day24;
+
+import nl.q8p.aoc2022.Assignment;
+import nl.q8p.aoc2022.Day;
+
+public class Day24 implements Day {
+
+    @Override
+    public Assignment first() {
+        return (run, input) -> "";
+    }
+
+    @Override
+    public Assignment second() {
+        return (run, input) -> "";
+    }
+}
