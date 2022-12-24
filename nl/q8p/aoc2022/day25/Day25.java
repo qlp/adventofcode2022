8ed1ea9c4c3f7e5bcd12f73ccd5a61552d0e7c90
Jurriaan Pruijs <jurriaan@pruijs.nl>
2022-12-24 07:57:01 +0100

    Day 25 - prepare

---
+++ nl/q8p/aoc2022/day25/Day25.java
@@ -0,0 +1,21 @@
+package nl.q8p.aoc2022.day25;
+
+import nl.q8p.aoc2022.Assignment;
+import nl.q8p.aoc2022.Day;
+
+import java.util.logging.Logger;
+
+public class Day25 implements Day {
+
+    private static final Logger LOG = Logger.getLogger(Day25.class.getName());
+
+    @Override
+    public Assignment first() {
+        return (run, input) -> "";
+    }
+
+    @Override
+    public Assignment second() {
+        return (run, input) -> "";
+    }
+}
